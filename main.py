a65674743749530be74780d7411e0b94bef6752b
Jagadee-sh <jeevannobel@gmail.com>
2025-02-06 15:16:42 +0530

    new

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 import get_pip
 import os
-
+from startScreen import startScreen 
 def install(package):
     subprocess.call([sys.executable, "-m", "pip", "install", package])
 
